b3eebca0726be6de02c3c535ef8805ff996d4afa
masanori.takeda <masanori.takeda@weserve.co.jp>
2019-10-11 16:55:45 +0900

    ゲーム継続判定修正

--- src/Game/BlackJack.java
+++ src/Game/BlackJack.java
@@ -12,9 +12,13 @@ public class BlackJack {
         Human d = new Human();
 
         //プレイヤーがゲーム終了を選択するか、所持金が0になると終了
-        while (continueGame || p.moneyInHand <= 0) {
+        while (continueGame && p.moneyInHand > 0) {
 
             continueGame = Game.startGame(deck, p, d, continueGame);
+        }
+
+        if (p.moneyInHand == 0) {
+            System.out.println("お金が無くなりました、、、");
         }
 
         //ゲーム終了
